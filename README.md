584a0c2edbd97b4bbdcae15d0042f82facec43ea
Karl Chan <karl-chan@bitbucket.org>
2022-10-09 14:53:33 +0100

    Handle bed & breakfast rule

--- README.md
+++ README.md
@@ -16,19 +16,19 @@ CGT due: £338.39
 =============================
 Breakdown:
 * Proceeds: £105531.44
-* Cost: £91539.52
+* Bed & Breakfast Cost: £0.00
+* Section 104 Holdings Cost: £91539.52
 * Net proceeds: £13991.93
 * Amount subject to CGT: £1691.93
 * CGT Rate: 20%
-* Net proceeds: £105193.06
 ```
 
 ## Done
+- [X] Bed & breakfast rule (https://www.gov.uk/government/publications/shares-and-capital-gains-tax-hs284-self-assessment-helpsheet/hs284-shares-and-capital-gains-tax-2019)
 - [X] Section 104 holding (https://www.gov.uk/tax-sell-shares/same-company)
 - [X] Exchange rate conversion
+- [X] Verify there are sufficient shares at sell date
 
 ## TODO
 - [ ] Same day sale
-- [ ] Bed & breakfast
 - [ ] Carry losses forwards
-- [ ] Verify there are sufficient shares at sell date
--- src/calculator.rs
+++ src/calculator.rs
@@ -5,12 +5,13 @@ use crate::{
     schwab::equity_award_center::EquityAwardCenter,
     stock::prices::{StockPriceProvider, YahooStockPriceProvider},
 };
-use chrono::NaiveDate;
+use chrono::{Duration, NaiveDate};
 
 pub struct CGTCalculatorResult {
     pub cgt: f64,
     pub proceeds: f64,
-    pub cost: f64,
+    pub bed_and_breakfast_cost: f64,
+    pub section_104_holding_cost: f64,
     pub amount_subject_to_cgt: f64,
     pub cgt_rate: f64,
 }
@@ -25,18 +26,18 @@ CGT due: £{:.2}
 =============================
 Breakdown:
 * Proceeds: £{:.2}
-* Cost: £{:.2}
+* Bed & Breakfast Cost: £{:.2}
+* Section 104 Holdings Cost: £{:.2}
 * Net proceeds: £{:.2}
 * Amount subject to CGT: £{:.2}
-* CGT Rate: {}%
-* Net proceeds: £{:.2}",
+* CGT Rate: {}%",
             self.cgt,
             self.proceeds,
-            self.cost,
-            self.proceeds - self.cost,
+            self.bed_and_breakfast_cost,
+            self.section_104_holding_cost,
+            self.proceeds - self.bed_and_breakfast_cost - self.section_104_holding_cost,
             self.amount_subject_to_cgt,
             self.cgt_rate * 100.0,
-            self.proceeds - self.cgt
         )
     }
 }
@@ -71,26 +72,52 @@ impl CGTCalculator {
         shares_to_sell: f64,
         sell_date: &NaiveDate,
     ) -> CGTCalculatorResult {
+        self.validate_sufficient_holdings_at_sell_date(symbol, shares_to_sell, sell_date);
+
         let proceeds = self.calculate_proceeds(symbol, shares_to_sell, sell_date);
-        let cost = self.calculate_cost(symbol, shares_to_sell, sell_date);
-        let amount_subject_to_cgt = (proceeds - cost - self.annual_exemption_amount).max(0.0);
+        let (bed_and_breakfast_cost, section_104_holding_cost) =
+            self.calculate_costs(symbol, shares_to_sell, sell_date);
+        let amount_subject_to_cgt = (proceeds
+            - bed_and_breakfast_cost
+            - section_104_holding_cost
+            - self.annual_exemption_amount)
+            .max(0.0);
         let cgt = amount_subject_to_cgt * self.cgt_rate;
 
         CGTCalculatorResult {
             cgt,
             proceeds,
-            cost,
+            bed_and_breakfast_cost,
+            section_104_holding_cost,
             amount_subject_to_cgt,
             cgt_rate: self.cgt_rate,
         }
     }
 
-    pub fn calculate_proceeds(
+    fn validate_sufficient_holdings_at_sell_date(
         &self,
         symbol: &str,
         shares_to_sell: f64,
         sell_date: &NaiveDate,
-    ) -> f64 {
+    ) {
+        let available_shares_at_sell_date: f64 = self
+            .equity_award_center
+            .awards
+            .iter()
+            .filter(|award| award.symbol == symbol)
+            .filter(|award| award.date_acquired <= sell_date.to_owned())
+            .map(|award| award.available_to_sell)
+            .sum();
+
+        if shares_to_sell > available_shares_at_sell_date {
+            panic!(
+                "You tried to sell {} {} shares, but there are only {} available before {}!",
+                shares_to_sell, symbol, available_shares_at_sell_date, sell_date
+            );
+        }
+    }
+
+    fn calculate_proceeds(&self, symbol: &str, shares_to_sell: f64, sell_date: &NaiveDate) -> f64 {
         let sell_price = self
             .stock_price_provider
             .get_historic_price(symbol, sell_date)
@@ -101,16 +128,106 @@ impl CGTCalculator {
             .expect(format!("Missing exchange rate for date: {:?}", sell_date).as_str())
     }
 
-    pub fn calculate_cost(&self, symbol: &str, shares_to_sell: f64, sell_date: &NaiveDate) -> f64 {
-        let records_before_sell_date = self
+    fn calculate_costs(
+        &self,
+        symbol: &str,
+        shares_to_sell: f64,
+        sell_date: &NaiveDate,
+    ) -> (f64, f64) {
+        let bed_and_breakfast_lookahead_date = sell_date.to_owned() + Duration::days(30);
+        let bed_and_breakfast_shares = self.count_bed_and_breakfast_shares(
+            symbol,
+            shares_to_sell,
+            sell_date,
+            &bed_and_breakfast_lookahead_date,
+        );
+        let section_104_holding_shares = shares_to_sell - bed_and_breakfast_shares;
+
+        (
+            self.calculate_bed_and_breakfast_cost(
+                symbol,
+                bed_and_breakfast_shares,
+                sell_date,
+                &bed_and_breakfast_lookahead_date,
+            ),
+            self.calculate_section_104_holding_cost(symbol, section_104_holding_shares, sell_date),
+        )
+    }
+
+    fn count_bed_and_breakfast_shares(
+        &self,
+        symbol: &str,
+        shares_to_sell: f64,
+        sell_date: &NaiveDate,
+        lookahead_date: &NaiveDate,
+    ) -> f64 {
+        let lookahead_shares: f64 = self
             .equity_award_center
             .awards
             .iter()
             .filter(|award| award.symbol == symbol)
-            .filter(|award| award.date_acquired <= sell_date.to_owned())
+            .filter(|award| {
+                sell_date.to_owned() <= award.date_acquired.to_owned()
+                    && award.date_acquired.to_owned() <= lookahead_date.to_owned()
+            })
+            .map(|award| award.available_to_sell)
+            .sum();
+
+        lookahead_shares.min(shares_to_sell)
+    }
+
+    fn calculate_bed_and_breakfast_cost(
+        &self,
+        symbol: &str,
+        shares_to_sell: f64,
+        sell_date: &NaiveDate,
+        lookahead_date: &NaiveDate,
+    ) -> f64 {
+        let mut lookahead_awards = self
+            .equity_award_center
+            .awards
+            .iter()
+            .filter(|award| award.symbol == symbol)
+            .filter(|award| {
+                sell_date.to_owned() <= award.date_acquired.to_owned()
+                    && award.date_acquired.to_owned() <= lookahead_date.to_owned()
+            })
+            .collect::<Vec<_>>();
+        lookahead_awards.sort_by_key(|award| award.date_acquired);
+
+        let mut total_cost = 0.0;
+        let mut remaining_shares_to_fill = shares_to_sell;
+        for award in lookahead_awards {
+            let shares_to_fill = remaining_shares_to_fill.min(award.available_to_sell);
+            total_cost += self
+                .exchange_rate_provider
+                .to_gbp(
+                    shares_to_fill * award.acquisition_price,
+                    &award.date_acquired,
+                )
+                .expect(
+                    format!("Missing exchange rate for date: {:?}", &award.date_acquired).as_str(),
+                );
+            remaining_shares_to_fill -= shares_to_fill;
+        }
+        total_cost
+    }
+
+    fn calculate_section_104_holding_cost(
+        &self,
+        symbol: &str,
+        shares_to_sell: f64,
+        sell_date: &NaiveDate,
+    ) -> f64 {
+        let awards_before_sell_date = self
+            .equity_award_center
+            .awards
+            .iter()
+            .filter(|award| award.symbol == symbol)
+            .filter(|award| award.date_acquired < sell_date.to_owned())
             .collect::<Vec<_>>();
 
-        let total_cost_before_sell_date: f64 = records_before_sell_date
+        let total_cost_before_sell_date: f64 = awards_before_sell_date
             .iter()
             .map(|award| {
                 self.exchange_rate_provider
@@ -124,7 +241,7 @@ impl CGTCalculator {
                     )
             })
             .sum();
-        let total_shares_before_sell_date: f64 = records_before_sell_date
+        let total_shares_before_sell_date: f64 = awards_before_sell_date
             .iter()
             .map(|award| award.available_to_sell)
             .sum();
